4f5e0878f8de43d14bffb37eaf38a1215843b2cf
xxavierargentino <xxavierargentino@hotmail.com>
2022-05-12 20:17:06 -0300

    continuando con pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,5 +10,9 @@ export class AppComponent {
   nombre: string = "FeRnAnDo";
 
   arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI: number = Math.PI;
+
+  porcentaje: number = 0.234;
 
 }
